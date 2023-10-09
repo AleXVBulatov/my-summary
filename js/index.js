7fd7eec42fabd483771e614d5e70372a3b8984b2
Alexey Bulatov <bulatov.a.v.89@gmail.com>
2023-10-09 15:12:12 +0300

    new photos

--- js/index.js
+++ js/index.js
@@ -1,10 +1,20 @@
 const photoRef = document.querySelector(".sidebar__photo img");
 const contactsRef = document.querySelector(".contacts__list");
+const aboutMeSectionRef = document.querySelector(".info__about-me");
+const sideBarPhotoRef = document.querySelector(".sidebar__photo");
 
 contactsRef.addEventListener("mouseenter", () => {
   photoRef.src = "img/my_photo_phone.jpg";
 });
 
 contactsRef.addEventListener("mouseleave", () => {
+  photoRef.src = "img/my_photo_thinking.jpg";
+});
+
+aboutMeSectionRef.addEventListener("mouseenter", () => {
+  photoRef.src = "img/my_photo.jpg";
+});
+
+sideBarPhotoRef.addEventListener("mouseenter", () => {
   photoRef.src = "img/my_photo.jpg";
 });
